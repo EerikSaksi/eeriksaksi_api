03c9b2196ad98a6ac6a8526091ce202c7e7004f6
Eerik <2392230s@student.gla.ac.uk>
2020-09-23 19:21:14 +0100

    now averages work

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const crypto = require('crypto');
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './db.sqlite',
   query: {
-    raw: true
+    raw: true,
   },
 });
 const SessionInfo = sequelize.define('SessionInfo', {
@@ -45,13 +45,13 @@ const SessionInfo = sequelize.define('SessionInfo', {
     allowNull: false,
     defaultValue: 0.0,
   },
-  'tunety.pe': {
+  'tunetype': {
     type: DataTypes.FLOAT,
     allowNull: false,
     defaultValue: 0.0,
   },
 });
-sequelize.sync({force: true})
+sequelize.sync({ force: false });
 
 const app = express();
 app.use(bodyParser.json());
@@ -59,31 +59,38 @@ app.use(cors());
 
 app.post('/session_id', async (req, res) => {
   var sessionID = crypto.randomBytes(20).toString('hex');
-  await SessionInfo.create({ sessionID })
-  res.send(sessionID)
+  await SessionInfo.create({ sessionID });
+  res.send(sessionID);
 });
 
 app.post('/send_session_info', async (req, res) => {
-  if (req.body && req.body.sessionID){
-    const {sessionID} = req.body
-    await SessionInfo.update(req.body, { where: {sessionID} });
-    res.sendStatus(200)
-  }
-  else{
-    res.sendStatus(404)
+  if (req.body && req.body.sessionID) {
+    const { sessionID } = req.body;
+    console.log(req.body);
+    await SessionInfo.update({...req.body, tunetype: req.body['tunety.pe']}, { where: { sessionID } });
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
   }
 });
-app.post('/averages', async (req, res) => {
-  res.send(
-    SessionInfo.findAll({
-      attributes: [[models.sequelize.fn('AVG', models.sequelize.col('Welcome')), 'Welcome'][(models.sequelize.fn('AVG', models.sequelize.col('Second Year')), 'Second Year')][(models.sequelize.fn('AVG', models.sequelize.col('UROS')), 'UROS')][(models.sequelize.fn('AVG', models.sequelize.col('Third Year')), 'Third Year')][(models.sequelize.fn('AVG', models.sequelize.col('Third Year Team Project')), 'Third Year Team Project')][(models.sequelize.fn('AVG', models.sequelize.col('tunety.pe')), 'tunety.pe')]],
-      group: ['venueId'],
-      order: [[models.sequelize.fn('AVG', models.sequelize.col('rating')), 'DESC']],
-    })
-  );
+app.get('/averages', async (req, res) => {
+  const averages = await SessionInfo.findOne({
+    attributes:       [
+      [sequelize.fn('AVG', sequelize.col('UROS')),'UROS'],
+      [sequelize.fn('AVG', sequelize.col('Welcome')),'Welcome'],
+      [sequelize.fn('AVG', sequelize.col('Timeline')),'Timeline'],
+      [sequelize.fn('AVG', sequelize.col('Second Year')),'Second Year'],
+      [sequelize.fn('AVG', sequelize.col('Third Year')),'Third Year'],
+      [sequelize.fn('AVG', sequelize.col('Third Year Team Project')),'Third Year Team Project'],
+      [sequelize.fn('AVG', sequelize.col('tunetype')),'tunety.pe'],
+    ],
+  });
+  debugger;
+  var response = {}
+  res.send(response);
 });
 
-const port  = process.env.PORT | 4000
+const port = process.env.PORT | 4000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
