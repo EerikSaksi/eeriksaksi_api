ad6ff982ff570b6fc23ec0d0dab2bb2b47c23bb7
Eerik <2392230s@student.gla.ac.uk>
2020-09-25 12:08:15 +0100

    Heroku

--- index.js
+++ index.js
@@ -45,13 +45,23 @@ const SessionInfo = sequelize.define('SessionInfo', {
     allowNull: false,
     defaultValue: 0.0,
   },
-  'tunetype': {
+  tunetype: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 0.0,
+  },
+  'Fourth Year': {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 0.0,
+  },
+  Analytics: {
     type: DataTypes.FLOAT,
     allowNull: false,
     defaultValue: 0.0,
   },
 });
-sequelize.sync({ force: false });
+sequelize.sync({ force: true });
 
 const app = express();
 app.use(bodyParser.json());
@@ -67,7 +77,7 @@ app.post('/send_session_info', async (req, res) => {
   if (req.body && req.body.sessionID) {
     const { sessionID } = req.body;
     console.log(req.body);
-    await SessionInfo.update({...req.body, tunetype: req.body['tunety.pe']}, { where: { sessionID } });
+    await SessionInfo.update({ ...req.body, tunetype: req.body['tunety.pe'] }, { where: { sessionID } });
     res.sendStatus(200);
   } else {
     res.sendStatus(404);
@@ -75,19 +85,19 @@ app.post('/send_session_info', async (req, res) => {
 });
 app.get('/averages', async (req, res) => {
   const averages = await SessionInfo.findOne({
-    attributes:       [
-      [sequelize.fn('AVG', sequelize.col('UROS')),'UROS'],
-      [sequelize.fn('AVG', sequelize.col('Welcome')),'Welcome'],
-      [sequelize.fn('AVG', sequelize.col('Timeline')),'Timeline'],
-      [sequelize.fn('AVG', sequelize.col('Second Year')),'Second Year'],
-      [sequelize.fn('AVG', sequelize.col('Third Year')),'Third Year'],
-      [sequelize.fn('AVG', sequelize.col('Third Year Team Project')),'Third Year Team Project'],
-      [sequelize.fn('AVG', sequelize.col('tunetype')),'tunety.pe'],
+    attributes: [
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('UROS')), 2), 'UROS'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Welcome')), 2), 'Welcome'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Timeline')), 2), 'Timeline'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Second Year')), 2), 'Second Year'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Third Year')), 2), 'Third Year'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Third Year Team Project')), 2), 'Third Year Team Project'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Fourth Year')), 2), 'Fourth Year'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('tunetype')), 2), 'tunety.pe'],
+      [sequelize.fn('ROUND', sequelize.fn('AVG', sequelize.col('Analytics')), 2), 'Analytics'],
     ],
   });
-  debugger;
-  var response = {}
-  res.send(response);
+  res.send(averages);
 });
 
 const port = process.env.PORT | 4000;
