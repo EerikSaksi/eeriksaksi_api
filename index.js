567ab711fb8d353968b01bdc31aebbb17abf3762
Eerik <2392230s@student.gla.ac.uk>
2020-09-29 13:08:46 +0100

    correct way around

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 4000 || process.env.PORT
+const port =  process.env.PORT || 4000 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
