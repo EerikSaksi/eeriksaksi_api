135ead2b007f07e0b64e470bbe2bd3e5f79cdd07
Eerik <2392230s@student.gla.ac.uk>
2020-09-29 12:57:27 +0100

    double or

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 4000 | process.env.PORT
+const port = 4000 || process.env.PORT
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
